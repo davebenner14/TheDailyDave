8e287d9f590c6f6164fdf6c06e98e9703985c511
David Benner <davidbenner@davids-mbp.lan>
2023-04-14 11:16:35 -0400

    added layout.blade.php

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>My Blog</title>
-        <link rel="stylesheet" href="/app.css">
-    </head>
-    <body>
-        @foreach ($posts as $post)
-            <article>
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">
-                        {{ $post->title }}
-                    </a>
-                </h1>
-                <div>
-                    {{ $post->excerpt }}
-                </div>
-            </article>
-        @endforeach
-    </body>
-</html>
+@extends('layout')
+
+@section('content')
+    @foreach ($posts as $post)
+        <article>
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
+            </h1>
+            <div>
+                {{ $post->excerpt }}
+            </div>
+        </article>
+    @endforeach
+@endsection
