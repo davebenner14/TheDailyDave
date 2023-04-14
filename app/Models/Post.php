c38b7c9ab75ac6901e7ef79f9b17ba9f1c29e487
David Benner <davidbenner@davids-mbp.lan>
2023-04-14 16:52:52 -0400

    added MySQL and changed some areas to help better suit database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,63 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title;
+    use HasFactory;
 
-    public $excerpt;
-
-    public $date;
-
-    public $body;
-
-    public $slug;
-
-    public function __construct($title, $excerpt, $date, $body, $slug)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all()
-    {
-        return cache()->rememberForever('posts.all', function () {
-            return collect(File::files(resource_path('posts')))
-                ->map(function ($file) {
-                    $document = YamlFrontMatter::parseFile($file);
-                    return new Post(
-                        $document->title,
-                        $document->excerpt,
-                        $document->date,
-                        $document->body(),
-                        $document->slug
-                    );
-                })
-                ->sortByDesc('date');
-        });
-    }
-
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug', $slug);
-
-    }
-    public static function findOrFail($slug)
-    {
-        $post = static::find($slug);
-
-        if (!$post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
-    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('posts/{post}', function ($slug) {
+Route::get('posts/{post}', function (Post $post) {
     return view('post', [
-        'post' => Post::findOrFail($slug)
+        'post' => $post
     ]);
 });
