7a6697de47e8a534782f12a470ab4bcdf0e5c457
David Benner <davidbenner@davids-mbp.lan>
2023-04-18 15:49:34 -0400

    added registration with password

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,13 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
