776a87d7db8981c3fe517a772a3f5956591d6655
David Benner <davidbenner@davids-mbp.lan>
2023-04-19 15:50:48 -0400

    added mailchimp api and subscribe option

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,49 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us6'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'JeffreyWay';
+        });
+
+        Blade::if('admin', function () {
+            return request()->user()?->can('admin');
+        });
     }
 }
