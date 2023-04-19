6ea4198f525410840dc82cc64273c10f48614ee5
David Benner <davidbenner@davids-mbp.lan>
2023-04-19 15:40:02 -0400

    updated post comments and post controllers

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -3,68 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
 
-class AdminPostController extends Controller
+class PostCommentsController extends Controller
 {
-    public function index()
+    public function store(Post $post)
     {
-        return view('admin.posts.index', [
-            'posts' => Post::paginate(50)
+        request()->validate([
+            'body' => 'required'
         ]);
-    }
 
-    public function create()
-    {
-        return view('admin.posts.create');
-    }
-
-    public function store()
-    {
-        Post::create(array_merge($this->validatePost(), [
+        $post->comments()->create([
             'user_id' => request()->user()->id,
-            'thumbnail' => request()->file('thumbnail')->store('thumbnails')
-        ]));
-
-        return redirect('/');
-    }
-
-    public function edit(Post $post)
-    {
-        return view('admin.posts.edit', ['post' => $post]);
-    }
-
-    public function update(Post $post)
-    {
-        $attributes = $this->validatePost($post);
-
-        if ($attributes['thumbnail'] ?? false) {
-            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
-        }
-
-        $post->update($attributes);
-
-        return back()->with('success', 'Post Updated!');
-    }
-
-    public function destroy(Post $post)
-    {
-        $post->delete();
-
-        return back()->with('success', 'Post Deleted!');
-    }
-
-    protected function validatePost(?Post $post = null): array
-    {
-        $post ??= new Post();
-
-        return request()->validate([
-            'title' => 'required',
-            'thumbnail' => $post->exists ? ['image'] : ['required', 'image'],
-            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post)],
-            'excerpt' => 'required',
-            'body' => 'required',
-            'category_id' => ['required', Rule::exists('categories', 'id')]
+            'body' => request('body')
         ]);
+
+        return back();
     }
 }
