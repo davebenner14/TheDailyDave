64ff284821946a01d16dc42c08d9398a267bfb81
David Benner <davidbenner@davids-mbp.lan>
2023-04-19 15:23:47 -0400

    updated comment section

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,8 +9,6 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
-
     protected $with = ['category', 'author'];
 
     public function scopeFilter($query, array $filters)
@@ -33,6 +31,11 @@ class Post extends Model
                 $query->where('username', $author)
             )
         );
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 
     public function category()
